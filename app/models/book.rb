bc059e35aa407b4880d0392e7bdddd1739602c44
Shota Furukawa <furukawa-shota@outlook.jp>
2016-07-10 14:56:47 +0900

    15章変更点

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
+  has_many :reviews
+  has_many :publishes
   validates_length_of :isbn, :maximum => 13
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613065956) do
+ActiveRecord::Schema.define(version: 20160706013100) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20160613065956) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "book_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
