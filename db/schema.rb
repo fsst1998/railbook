f97ff1ce0839935d8a478fc27293a3fc5c1bd2b2
Shota Furukawa <furukawa-shota@outlook.jp>
2016-07-11 17:53:12 +0900

    15章修正＆変更点A完了

--- db/schema.rb
+++ db/schema.rb
@@ -17,19 +17,11 @@ ActiveRecord::Schema.define(version: 20160706013100) do
     t.string   "isbn"
     t.string   "title"
     t.integer  "price"
-    t.string   "publish"
+    t.string   "publish_id"
     t.date     "published"
     t.boolean  "cd"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "listings", force: :cascade do |t|
-    t.string   "publish"
-    t.string   "publishname"
-    t.string   "address"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "publishes", force: :cascade do |t|
